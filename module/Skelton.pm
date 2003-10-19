ce4305b7db6cabb88b763d78959b18d6245b292c
Topia <topia@clovery.jp>
2003-10-19 12:30:09 +0000

    * merge from vendor repository.

---
+++ module/Skelton.pm
@@ -0,0 +1,145 @@
+# -----------------------------------------------------------------------------
+# $Id: Skelton.pm,v 1.1 2003/10/19 12:30:23 topia Exp $
+# -----------------------------------------------------------------------------
+# モジュールのスケルトン。
+# -----------------------------------------------------------------------------
+package Skelton;
+use strict;
+use warnings;
+use base qw(Module);
+
+sub new {
+    my $class = shift;
+    # モジュールが必要になった時に呼ばれる。
+    # これはモジュールのコンストラクタである。
+    # 引数は無し。
+    my $this = $class->SUPER::new;
+
+    return $this;
+}
+
+sub destruct {
+    my $this = shift;
+    # モジュールが不要になった時に呼ばれる。
+    # これはモジュールのデストラクタである。このメソッドが呼ばれた後はDESTROYを除いて
+    # いかなるメソッドも呼ばれる事が無い。タイマーを登録した場合は、このメソッドが
+    # 責任を持ってそれを解除しなければならない。
+    # 引数は無し。
+}
+
+sub message_arrived {
+    my ($this,$message,$sender) = @_;
+    # サーバーまたはクライアントからメッセージが来た時に呼ばれる。
+    # 戻り値はIRCMessageまたはその配列またはundef。
+    #
+    # $message :
+    #    内容: IRCMessageオブジェクト
+    #    サーバーから、またはクライアントから送られてきたメッセージ。
+    #    モジュールはこのオブジェクトをそのまま返しても良いし、
+    #    改変して返しても良いし何も返さなくても良いし二つ以上返しても良い。
+    # $sender :
+    #    内容: IrcIOオブジェクト
+    #    このメッセージを発したIrcIO。サーバーまたはクライアントである。
+    #    メッセージがサーバーから来たのかクライアントから来たのかは
+    #    $sender->isa('IrcIO::Server')などとすれば判定出来る。
+    #
+    # サーバー→クライアントの流れでも、Prefixを持たないメッセージを
+    # 流しても構わない。逆に言えば、そのようなメッセージが来ても
+    # 問題が起こらないようにモジュールを設計しなければならない。
+    return $message;
+}
+## Auto::Utils::generate_reply_closures を使う場合。
+# sub message_arrived {
+#     my ($this,$message,$sender) = @_;
+#     my @result = ($msg);
+# 
+#     if ($msg->command eq 'PRIVMSG') {
+# 	my ($reply,$reply_as_priv,$get_raw_ch_name,$reply_anywhere,$get_full_ch_name)
+# 	    = Auto::Utils::generate_reply_closures($msg,$sender,\@result);
+# 
+# 	$reply_anywhere->('Hello, #(name|default_name)',
+# 			'default_name' => '(your name)');
+# 	if ($get_raw_ch_name->() eq '#Tiarra_testing') {
+# 	    # なんらかの処理
+# 	}
+# 	if ($get_full_ch_name->() eq '#Tiarra_testing@LocalServer') {
+# 	    # なんらかの処理
+# 	}
+#     }
+#     return @result;
+# }
+# 
+
+sub client_attached {
+    my ($this,$client) = @_;
+    # クライアントが新規に接続した時に呼ばれる。
+    # 戻り値は無し。
+    #
+    # $client :
+    #    内容: IrcIO::Clientオブジェクト
+    #    接続されたクライアント。
+}
+
+sub client_detached {
+    my ($this,$client) = @_;
+    # クライアントが切断した時に呼ばれる。
+    # 戻り値は無し。
+    #
+    # $client :
+    #    内容: IrcIO::Clientオブジェクト
+    #    切断したクライアント。
+}
+
+sub connected_to_server {
+    my ($this,$server,$new_connection) = @_;
+    # サーバーに接続した時に呼ばれる。
+    # 戻り値は無し。
+    #
+    # $server :
+    #    内容: IrcIO::Serverオブジェクト
+    #         接続したサーバー。
+    # $new_connection :
+    #    内容: 真偽値
+    #         新規の接続なら1。切断後の自動接続ではundef。
+}
+
+sub disconnected_from_server {
+    my ($this,$server) = @_;
+    # サーバーから切断した(或いはされた)時に呼ばれる。
+    # 戻り値は無し。
+    #
+    # $server :
+    #    内容: IrcIO::Serverオブジェクト
+    #         切断したサーバー。
+}
+
+sub notification_of_message_io {
+    my ($this,$message,$io,$type) = @_;
+    # サーバーから受け取ったメッセージ、サーバーに送ったメッセージ、
+    # クライアントから受け取ったメッセージ、クライアントに送ったメッセージは
+    # このメソッドで各モジュールに通知される。通知されるだけで変更する事は出来ない。
+    # 戻り値は無し。
+    #
+    # $message :
+    #    内容: IRCMessageオブジェクト
+    #         送受信されたメッセージ
+    # $io :
+    #    内容: IrcIO::Server又はIrcIO::Clientオブジェクト
+    #         送受信が行なわれたIrcIO
+    # $type :
+    #    内容: 文字列
+    #         'in'なら受信、'out'なら送信
+}
+
+sub control_requested {
+    my ($this,$request) = @_;
+    # 外部コントロールプログラムからのメッセージが来た。
+    # 戻り値はControlPort::Reply。
+    #
+    # $request:
+    #    内容 : ControlPort::Request
+    #          送られたリクエスト
+    die "This module doesn't support controlling.\n";
+}
+
+1;
