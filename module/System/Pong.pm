3f9ea9b8620923bc91e76648de75f7b900d1ad0d
Topia <topia@clovery.jp>
2004-02-21 14:49:28 +0000

    * NumericReply-ize.
    * do not pass server's or client's PING/PONG each other.
    
    * fix messages.

--- module/System/Pong.pm
+++ module/System/Pong.pm
@@ -5,27 +5,60 @@
 package System::Pong;
 use strict;
 use warnings;
+use Configuration;
+use NumericReply;
 use base qw(Module);
 
 sub message_arrived {
     my ($this,$message,$sender) = @_;
     
     if ($message->command eq 'PING') {
-	my ($prefix);
-	if ($sender->isa('IrcIO::Server')) {
-	    $prefix = undef;
+	my ($prefix) = do {
+	    if ($sender->isa('IrcIO::Server')) {
+		undef;
+	    } else {
+		Configuration->shared->general->sysmsg_prefix;
+	    }
+	};
+	my ($nick) = do {
+	    if ($sender->isa('IrcIO::Server')) {
+		$sender->current_nick;
+	    } else {
+		RunLoop->shared_loop->current_nick;
+	    }
+	};
+	if ($message->n_params < 1) {
+	    # これを送りつけてきたサーバー/クライアントにエラーを返す。
+	    $sender->send_message(
+		new IRCMessage(
+		    (defined $prefix ? (Prefix => $prefix) : ()),
+		    Command => ERR_NOORIGIN,
+		    Params => [
+			$nick,
+			'No origin specified',
+		       ]));
 	} else {
-	    $prefix = $message->param(0);
+	    my ($target);
+	    if ($sender->isa('IrcIO::Server')) {
+		$nick = undef;
+		$target = $sender->server_hostname;
+	    } else {
+		$target = Configuration->shared->general->sysmsg_prefix;
+	    }
+	    # これを送りつけてきたサーバー/クライアントにPONGを送り返す。
+	    $sender->send_message(
+		new IRCMessage(
+		    ((defined $prefix) ? (Prefix => $prefix) : ()),
+		    Command => 'PONG',
+		    Params => [
+			$target,
+			(defined $nick ? $nick : ()),
+		       ]));
 	}
-	# これを送りつけてきたサーバー/クライアントにPONGを送り返す。
-	$sender->send_message(
-	    new IRCMessage(
-		Command => 'PONG',
-		Params => $message->params));
-	
 	# print "System::Pong ponged to ".$message->params->[0].".\n";
 	
-	return $message;
+	# PINGメッセージはこれ以上伝達させず、ここで消してしまう。
+	return undef;
     }
     elsif ($message->command eq 'PONG') {
 	# PONGメッセージはこれ以上伝達させず、ここで消してしまう。
