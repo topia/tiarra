324ca929f8c206befd169f270a46cedadf51a069
Topia <topia@clovery.jp>
2004-08-28 10:13:10 +0000

    * add append_spec to REDIR_STD{OUT,ERR}.

--- common/run-script/rc-onces/remove-log.sh
+++ common/run-script/rc-onces/remove-log.sh
@@ -3,6 +3,13 @@
 # copyright (C) 2004 Topia <topia@clovery.jp>. all rights reserved.
 
 # FIXME: fixed string
-REDIR_STDOUT=${REDIR_STDOUT:-errlog.stdout}
-REDIR_STDERR=${REDIR_STDERR:-errlog.stderr}
-rm -f "${REDIR_STDOUT}" "${REDIR_STDERR}"
+REDIR_STDOUT="${REDIR_STDOUT:->errlog.stdout}"
+REDIR_STDERR="${REDIR_STDERR:->errlog.stderr}"
+for i in "${REDIR_STDOUT}" "${REDIR_STDERR}"; do
+  case "$i" in
+    \&*|-) ;;
+    *)
+      rm -f "${i#>}"
+      ;;
+  esac
+done
