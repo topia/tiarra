41b8e520f12fbf87c2c21971247c2fd5ed8f2b5c
Topia <topia@clovery.jp>
2004-09-07 09:58:05 +0000

    * add usage comment.

--- main/Tiarra/SharedMixin.pm
+++ main/Tiarra/SharedMixin.pm
@@ -1,13 +1,29 @@
 # -----------------------------------------------------------------------------
 # $Id$
 # -----------------------------------------------------------------------------
-# Shared Instance Mixin
+# Shared Instance(Singleton) Mixin
 # -----------------------------------------------------------------------------
 # copyright (C) 2004 Topia <topia@clovery.jp>. all rights reserved.
 package Tiarra::SharedMixin;
 use strict;
 use warnings;
 our $ExportLevel = 0;
+
+# usage:
+#  use Tiarra::SharedMixin;
+#  our $_shared_instance; # optional, but useful for documentation.
+#  *shared_module = \&shared; # alias
+#  sub _new {
+#      my $class = shift;
+#      my ($this) = {};
+#      bless $this, $class;
+#      #__PACKAGE__->shared->some_func; # can't use
+#      return $this;
+#  }
+#  sub _initialize { # optional
+#      my $this = shift;
+#      __PACKAGE__->shared->some_func; # OK
+#  }
 
 # use $_shared_instance variable.
 # import shared and _this functions.
