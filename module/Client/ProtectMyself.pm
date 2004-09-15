85c0a781e45963af1650b9829812511211626e6e
Topia <topia@clovery.jp>
2004-09-15 13:05:15 +0000

    * use AliasDB->stdreplace.

--- module/Client/ProtectMyself.pm
+++ module/Client/ProtectMyself.pm
@@ -9,10 +9,13 @@ use base qw(Module);
 use Multicast;
 use RunLoop;
 use IRCMessage;
+use Auto::AliasDB;
+use Tiarra::Utils;
+my $utils = Tiarra::Utils->shared;
 
 sub message_arrived {
     my ($this,$msg,$sender) = @_;
-    my $runloop = RunLoop->shared_loop;
+    my $runloop = $this->_runloop;
     my $current_nick = $runloop->current_nick;
 
     if ($runloop->multi_server_mode_p &&
@@ -28,7 +31,7 @@ sub message_arrived {
 
 sub message_io_hook {
     my ($this,$msg,$io,$type) = @_;
-    my $runloop = RunLoop->shared_loop;
+    my $runloop = $this->_runloop;
     my $current_nick = $runloop->current_nick;
 
     if ($runloop->multi_server_mode_p &&
@@ -37,7 +40,7 @@ sub message_io_hook {
 		    $msg->remark('message-send-by-other') &&
 			defined $msg->nick &&
 			    $msg->nick eq $current_nick) {
-	my ($msg_tmpl, @affected);
+	my ($msg_tmpl, %additional_replaces, @affected);
 	my $attach_for_client = sub {
 	    my $network_name = $msg->remark(__PACKAGE__ . '/network-name');
 	    $network_name = $runloop->default_network
@@ -55,63 +58,43 @@ sub message_io_hook {
 	    }
 	};
 	if ($msg->command eq 'NICK') {
-	    $msg_tmpl = IRCMessage->new(
-		Command => 'DUMMY', # set later
-		Params => [
-		    undef, # set later
-		    join(' ',
-			 'Nick changed',
-			 $msg->nick,
-			 '->',
-			 $msg->param(0),
-			)],
-	       );
+	    $msg_tmpl = $utils->get_first_defined(
+		$this->config->nick_format,
+		'Nick changed #(nick.now) -> #(nick.new)');
+	    $additional_replaces{'nick.new'} = $msg->param(0);
 	    $set_affected_by_remark->();
 	} elsif ($msg->command eq 'PART') {
-	    $msg_tmpl = IRCMessage->new(
-		Command => 'DUMMY', # set later
-		Params => [
-		    undef, # set later
-		    join(' ',
-			 'Part',
-			 $msg->nick,
-			 '('.$msg->param(1).')',
-			)],
-	       );
+	    $msg_tmpl = $utils->get_first_defined(
+		$this->config->part_format,
+		'Part #(nick.now) (#(message)) from #(target)');
+	    $additional_replaces{'message'} = $msg->param(1);
 	    @affected = $msg->param(0);
 	} elsif ($msg->command eq 'QUIT') {
-	    $msg_tmpl = IRCMessage->new(
-		Command => 'DUMMY', # set later
-		Params => [
-		    undef, # set later
-		    join(' ',
-			 'Quit',
-			 $msg->nick,
-			 '('.$msg->param(0).')',
-			)],
-	       );
+	    $msg_tmpl = $utils->get_first_defined(
+		$this->config->quit_format,
+		'Quit #(nick.now) (#(message))');
+	    $additional_replaces{'message'} = $msg->param(0);
 	    $set_affected_by_remark->();
 	} elsif ($msg->command eq 'JOIN') {
-	    $msg_tmpl = IRCMessage->new(
-		Command => 'DUMMY', # set later
-		Params => [
-		    undef, # set later
-		    join(' ',
-			 'Join',
-			 $msg->nick,
-			 'to',
-			 $msg->param(0),
-			)],
-	       );
+	    $msg_tmpl = $utils->get_first_defined(
+		$this->config->join_format,
+		'Join #(nick.now) (#(prefix.now)) to #(target)');
 	    @affected = $msg->param(0);
 	}
 	if (@affected) {
-	    $msg_tmpl->prefix(
-		$runloop->sysmsg_prefix(qw(system fake::system)));
-	    $msg_tmpl->command('NOTICE');
+	    my $aliasdb = Auto::AliasDB->shared;
+	    my $msg_skel = IRCMessage->new(
+		Prefix => $runloop->sysmsg_prefix(qw(system fake::system)),
+		Command => 'NOTICE',
+		Params => [undef, undef]);
 	    return map {
-		my $new_msg = $msg_tmpl->clone(deep => 1);
+		my $new_msg = $msg_skel->clone;
 		$new_msg->param(0, $_);
+		$new_msg->param(1, $aliasdb->stdreplace(
+		    $msg->prefix, $msg_tmpl, $msg, undef,
+		    target => $_,
+		    %additional_replaces,
+		   ));
 		$new_msg;
 	    } @affected;
 	}
@@ -124,6 +107,16 @@ sub message_io_hook {
 info: IRC メッセージにちょっと変更を加えて、クライアントのバグを抑制する
 default: off
 
-# オプションはありません。
+# {nick,part,quit,join}-format: それぞれのメッセージのフォーマットを指定します。
+# {nick,user,host,prefix}.now などはどこでも使えます。
+# そのほかには
+#  target   : 表示するチャンネル(またはニック)。
+#  nick.new : nick-format のみ。新しいニック。
+#  message  : part と quit 。メッセージ。
+
+nick-format: Nick changed #(nick.now) -> #(nick.new)
+part-format: Part #(nick.now) (#(message)) from #(target)
+quit-format: Quit #(nick.now) (#(message))
+join-format: Join #(nick.now) (#(prefix.now)) to #(target)
 
 =cut
