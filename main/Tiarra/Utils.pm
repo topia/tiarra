011847489fc3d6de50e38ce1d007e0780c4cdba4
Topia <topia@clovery.jp>
2004-09-19 10:02:00 +0000

    * add _parse_{array_,}_attr_define to split common functionality.
    * add define_array_attr_{accessor,getter,setter}.
    
    * add define_{proxy,enum}.
    
    * add call_with_wantarray.
    
    * add do_with_ensure.

--- main/Tiarra/Utils.pm
+++ main/Tiarra/Utils.pm
@@ -45,20 +45,27 @@ sub define_function {
     undef;
 }
 
+sub _parse_attr_define {
+    shift; # drop
+    shift; # drop
+    my $value = shift;
+
+    if (ref($value) eq 'ARRAY') {
+	$value;
+    } else {
+	[$value, $value];
+    }
+}
+
 sub define_attr_accessor {
     my $pkg = shift;
-    my $class_method = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
     foreach (@_) {
-	my ($valname, $funcname);
-	if (ref($_) eq 'ARRAY') {
-	    $funcname = $_->[0];
-	    $valname = $_->[1];
-	} else {
-	    $funcname = $valname = $_;
-	}
+	my ($funcname, $valname) = @{$pkg->_parse_attr_define($call_pkg, $_)};
 	$pkg->define_function(
-	    $pkg->get_package,
-	    ($class_method ? sub {
+	    $call_pkg,
+	    ($class_method_p ? sub {
 		 my ($class_or_this, $value) = @_;
 		 my $this = $class_or_this->_this;
 		 $this->{$valname} = $value if defined $value;
@@ -75,18 +82,13 @@ sub define_attr_accessor {
 
 sub define_attr_getter {
     my $pkg = shift;
-    my $class_method = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
     foreach (@_) {
-	my ($valname, $funcname);
-	if (ref($_) eq 'ARRAY') {
-	    $funcname = $_->[0];
-	    $valname = $_->[1];
-	} else {
-	    $funcname = $valname = $_;
-	}
+	my ($funcname, $valname) = @{$pkg->_parse_attr_define($call_pkg, $_)};
 	$pkg->define_function(
-	    $pkg->get_package,
-	    ($class_method ? sub {
+	    $call_pkg,
+	    ($class_method_p ? sub {
 		 shift->_this->{$valname};
 	     } : sub {
 		 shift->{$valname};
@@ -97,23 +99,134 @@ sub define_attr_getter {
 
 sub define_attr_setter {
     my $pkg = shift;
-    my $class_method = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
     foreach (@_) {
-	my ($valname, $funcname);
-	if (ref($_) eq 'ARRAY') {
-	    $funcname = $_->[0];
-	    $valname = $_->[1];
-	} else {
-	    $funcname = $valname = $_;
-	}
+	my ($funcname, $valname) = @{$pkg->_parse_attr_define($call_pkg, $_)};
 	$pkg->define_function(
-	    $pkg->get_package,
-	    ($class_method ? sub {
+	    $call_pkg,
+	    ($class_method_p ? sub {
 		 shift->_this->{$valname} = shift;
 	     } : sub {
 		 shift->{$valname} = shift;
 	     }),
 	    $funcname);
+    }
+}
+
+sub _parse_array_attr_define {
+    shift; # drop
+    my $call_pkg = shift;
+
+    my $value = shift;
+    if (ref($value) eq 'ARRAY') {
+	$value;
+    } else {
+	my $funcname = $value;
+	my $index = uc($funcname);
+	$index = $call_pkg->$index;
+	[$funcname, $index];
+    }
+}
+
+sub define_array_attr_accessor {
+    my $pkg = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
+    foreach (@_) {
+	my ($funcname, $index) =
+	    @{$pkg->_parse_array_attr_define($call_pkg, $_)};
+	$pkg->define_function(
+	    $call_pkg,
+	    ($class_method_p ? sub {
+		 my ($class_or_this, $value) = @_;
+		 my $this = $class_or_this->_this;
+		 $this->[$index] = $value if defined $value;
+		 return $this->[$index];
+	     } : sub {
+		 my ($this, $value) = @_;
+		 $this->[$index] = $value if defined $value;
+		 return $this->[$index];
+	     }),
+	    $funcname);
+    }
+    undef;
+}
+
+sub define_array_attr_getter {
+    my $pkg = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
+    foreach (@_) {
+	my ($funcname, $index) =
+	    @{$pkg->_parse_array_attr_define($call_pkg, $_)};
+	$pkg->define_function(
+	    $call_pkg,
+	    ($class_method_p ? sub {
+		 shift->_this->[$index];
+	     } : sub {
+		 shift->[$index];
+	     }),
+	    $funcname);
+    }
+}
+
+sub define_array_attr_setter {
+    my $pkg = shift;
+    my $class_method_p = shift;
+    my $call_pkg = $pkg->get_package;
+    foreach (@_) {
+	my ($funcname, $index) =
+	    @{$pkg->_parse_array_attr_define($call_pkg, $_)};
+	$pkg->define_function(
+	    $call_pkg,
+	    ($class_method_p ? sub {
+		 shift->_this->[$index] = shift;
+	     } : sub {
+		 shift->[$index] = shift;
+	     }),
+	    $funcname);
+    }
+}
+
+sub define_proxy {
+    my $pkg = shift;
+    my $proxy_target_funcname = shift;
+    my $class_method_p = shift;
+    foreach (@_) {
+	my ($funcname, $proxyname);
+	if (ref($_) eq 'ARRAY') {
+	    $funcname = $_->[0];
+	    $proxyname = $_->[1];
+	} else {
+	    $funcname = $proxyname = $_;
+	}
+	$pkg->define_function(
+	    $pkg->get_package,
+	    ($class_method_p ? sub {
+		 shift->_this->$proxy_target_funcname->$proxyname(@_);
+	     } : sub {
+		 shift->$proxy_target_funcname->$proxyname(@_);
+	     }),
+	    $funcname);
+    }
+}
+
+sub define_enum {
+    my $pkg = shift;
+    my $i = 0;
+    foreach (@_) {
+	my (@funcnames);
+	if (ref($_) eq 'ARRAY') {
+	    @funcnames = @$_;
+	} else {
+	    @funcnames = $_;
+	}
+	$pkg->define_function(
+	    $pkg->get_package,
+	    sub () { $i; },
+	    @funcnames);
+	++$i;
     }
 }
 
@@ -151,6 +264,42 @@ sub get_first_defined {
 	return $_ if defined $_;
     }
     return undef;
+}
+
+sub call_with_wantarray {
+    shift; # drop
+    my ($wantarray, $closure, @args) = @_;
+
+    if (!defined $wantarray) {
+	# void context
+	$closure->(@args);
+	return undef;
+    } elsif (!$wantarray) {
+	# scalar context
+	my $ret = $closure->(@args);
+	return $ret;
+    } else {
+	# list context
+	my $ret = [$closure->(@args)];
+	return @$ret;
+    }
+}
+
+sub do_with_ensure {
+    shift; # drop
+    my ($closure, $ensure, @args) = @_;
+    my $retval;
+
+    eval {
+	$retval = [$closure->(@args)];
+    };
+    my $error = $@;
+    $ensure->();
+    if ($error) {
+	die $error;
+    } else {
+	return @$retval;
+    }
 }
 
 1;
