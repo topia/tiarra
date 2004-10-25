1769da7142cc034dae74d0b1ef510797667cf9f2
Topia <topia@clovery.jp>
2004-10-25 22:49:21 +0000

    * cosmetic fixes.
    * return $default even if $default not defined.

--- main/Tiarra/Utils.pm
+++ main/Tiarra/Utils.pm
@@ -37,7 +37,7 @@ sub cond_yesno {
     shift; # drop
     my ($value, $default) = @_;
 
-    return $default || 0 unless defined $value;
+    return $default unless defined $value;
     return 0 if ($value =~ /[fn]/i); # false/no
     return 1 if ($value =~ /[ty]/i); # true/yes
     return 1 if ($value); # ¿ôÃÍÈ½Äê
