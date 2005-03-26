2e938d5d22f1580f0cdc872c534dfd53932eb423
Topia <topia@clovery.jp>
2005-03-26 13:30:27 +0000

    * rewrite. add extract_from_text, make_text, dequote and quote (as class method).

--- main/CTCP.pm
+++ main/CTCP.pm
@@ -8,7 +8,7 @@ use strict;
 use warnings;
 use Carp;
 use UNIVERSAL;
-use IRCMessage;
+use base qw(Tiarra::IRC::NewMessageMixin);
 
 use SelfLoader;
 1;
@@ -24,66 +24,20 @@ sub extract {
     if (!defined $msg) {
 	croak "CTCP::extract, Arg[0] is undef.\n";
     }
-    if (!UNIVERSAL::isa($msg,'IRCMessage')) {
+    if (!UNIVERSAL::isa($msg, __PACKAGE__->irc_message_class)) {
 	croak "CTCP::extract, Arg[0] is bad ref: ".ref($msg)."\n";
     }
 
-    my $low_level_dequote = sub {
-	my ($symbol) = @_;
-
-	if ($symbol eq '0') {
-	    return "\x00";
-	} elsif ($symbol eq 'n') {
-	    return "\x0a";
-	} elsif ($symbol eq 'r') {
-	    return "\x0d";
-	} elsif ($symbol eq "\x10") {
-	    return "\x10";
-	} else {
-	    # error, but return.
-	    return $symbol;
-	}
-    };
-
-    my $ctcp_level_dequote = sub {
-	my ($symbol) = @_;
-
-	if ($symbol eq 'a') {
-	    return "\x01";
-	} elsif ($symbol eq "\x5c") {
-	    return "\x5c";
-	} else {
-	    # error, but return.
-	    return $symbol;
-	}
-    };
-
-    my @result;
     if ($msg->command eq 'PRIVMSG' || $msg->command eq 'NOTICE') {
-	@result = map {
-	    # 2nd Level
-	    s/\x10(.)/$low_level_dequote->($1)/eg;
-
-	    # 1st Level
-	    s/\x5c(.)/$ctcp_level_dequote->($1)/eg;
-
-	    $_;
-	} ($msg->param(1) =~ m/\x01(.*)\x01/g);
-    }
-
-    if (wantarray) {
-	@result;
-    }
-    else {
-	$result[0];
+	__PACKAGE__->extract_from_text($msg->param(1));
     }
 }
 
 sub make {
-    # CTCPメッセージを含むIRCMessageを作って返す。
+    # CTCPメッセージを含むTiarra::IRC::Messageを作って返す。
     #
     # $message: 含めるCTCPメッセージ
-    # $target : 作るIRCMessageの最初のパラメータ。nickやチャンネル名を入れる。
+    # $target : 作るTiarra::IRC::Messageの最初のパラメータ。nickやチャンネル名を入れる。
     # $command: PRIVMSGとNOTICEのうち、どちらのコマンドで作るか。省略された場合はNOTICEになる。
     my ($message,$target,$command) = @_;
 
@@ -94,26 +48,92 @@ sub make {
 	$command = 'NOTICE';
     }
 
-    my $result = IRCMessage->new(
+    my $result = __PACKAGE__->construct_irc_message(
 	Command => $command,
 	Params => [$target,
-		   do {
-		       $_ = $message;
-
-		       # 1st Level
-		       s/\x5c/\x5c\x5c/g;
-		       s/\x01/\x5ca/g;
-
-		       # 2nd Level
-		       s/\x10/\x10\x10/g;
-		       s/\x00/\x100/g;
-		       s/\x0a/\x10n/g;
-		       s/\x0d/\x10r/g;
-
-		       "\x01$_\x01";
-		   }]);
+		   __PACKAGE__->make_text($message),
+		  ]);
 
     $result;
+}
+
+sub _low_level_dequote {
+    my ($symbol) = @_;
+
+    if ($symbol eq '0') {
+	return "\x00";
+    } elsif ($symbol eq 'n') {
+	return "\x0a";
+    } elsif ($symbol eq 'r') {
+	return "\x0d";
+    } elsif ($symbol eq "\x10") {
+	return "\x10";
+    } else {
+	# error, but return.
+	return $symbol;
+    }
+}
+
+sub _ctcp_level_dequote {
+    my ($symbol) = @_;
+
+    if ($symbol eq 'a') {
+	return "\x01";
+    } elsif ($symbol eq "\x5c") {
+	return "\x5c";
+    } else {
+	# error, but return.
+	return $symbol;
+    }
+}
+
+sub dequote {
+    shift; # drop
+    local $_ = shift;
+
+    # 2nd Level
+    s/\x10(.)/_low_level_dequote($1)/eg;
+
+    # 1st Level
+    s/\x5c(.)/_ctcp_level_dequote($1)/eg;
+
+    $_;
+}
+
+sub extract_from_text {
+    my $this = shift;
+    grep {
+	if (!wantarray) {
+	    return $_;
+	}
+	1;
+    } map {
+	$this->dequote($1);
+    } (shift =~ m/\x01(.*)\x01/g);
+}
+
+sub quote {
+    shift; # drop
+    local $_ = shift;
+    # 1st Level
+    s/\x5c/\x5c\x5c/g;
+    s/\x01/\x5ca/g;
+
+    # 2nd Level
+    s/\x10/\x10\x10/g;
+    s/\x00/\x100/g;
+    s/\x0a/\x10n/g;
+    s/\x0d/\x10r/g;
+
+    $_;
+}
+
+sub make_text {
+    my $this = shift;
+    my @ctcps = map {
+	"\x01" . $this->quote($_) . "x01";
+    } @_;
+    return wantarray ? @ctcps : join('', @ctcps);
 }
 
 1;
