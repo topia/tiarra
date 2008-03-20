4408a83f33a69ed76325e83ce21d758133a01d5c
hio <devnull@localhost>
2008-03-20 16:11:22 +0000

    lang/perl/tiarra: add ProgressCallback, and documents. fix, error handring on connect/disconnect failure. fix, content body after last CR-LF was lost.

--- module/Tools/HTTPClient.pm
+++ module/Tools/HTTPClient.pm
@@ -13,7 +13,7 @@ use RunLoop;
 use Timer;
 # 本当はHTTP::RequestとHTTP::Responseを使いたいが…
 
-my $DEBUG = 0;
+our $DEBUG = 0;
 
 sub new {
     my ($class, %args) = @_;
@@ -33,6 +33,7 @@ sub new {
     $this->{timeout} = $args{Timeout}; # undef可
 
     $this->{callback} = undef;
+    $this->{progress_callback} = undef;
     $this->{socket} = undef;
     $this->{hook} = undef;
     $this->{timeout_timer} = undef;
@@ -42,7 +43,7 @@ sub new {
     $this->{status_fetched} = undef;
     $this->{header_fetched} = undef;
 
-    $this->{reply} = {Header => {}, Content => ''};
+    $this->{reply} = {Header => {}, Content => '', StreamState => 'ready' };
 
     $this;
 }
@@ -61,10 +62,17 @@ sub start {
     # }
     # エラーが発生した場合はエラーメッセージ(文字列)が渡される。
     my ($this, $callback) = @_;
+    my %opts;
+    if( @_>=3 )
+    {
+      ($this, %opts) = @_;
+      $callback = $opts{Callback};
+    }
     if ($this->{callback}) {
 	croak "This client is already started";
     }
     $this->{callback} = $callback;
+    $this->{progress_callback} = $opts{ProgressCallback};
 
     if (!$callback or ref($callback) ne 'CODE') {
 	croak "Callback function is required";
@@ -99,11 +107,17 @@ sub start {
     }
 
     # 接続
+    $this->{reply}{StreamState} = 'connect';
     $this->{socket} = LinedINETSocket->new->connect($host, $port);
     if (!defined $this->{socket}) {
 	# 接続不可能
 	croak "Failed to connect: $host:$port";
     }
+    if (!defined $this->{socket}->sock) {
+	# 接続不可能
+	croak "Failed to connect: $host:$port, $@";
+    }
+    $this->{reply}{StreamState} = 'fetch_status';
 
     # 必要ならタイムアウト用のタイマーをインストール
     if ($this->{timeout}) {
@@ -151,8 +165,10 @@ sub _main {
 	return;
     }
 
+    my $progress;
     while (defined(my $line = $this->{socket}->pop_queue)) {
 	$DEBUG and print "< $line\n";
+	$progress = 1;
 	
 	if (!$this->{status_fetched}) {
 	    # ステータス行
@@ -162,6 +178,7 @@ sub _main {
 		$this->{reply}{Code} = $2;
 		$this->{reply}{Message} = $3;
 		$this->{status_fetched} = 1;
+		$this->{reply}{StreamState} = 'fetch_header';
 	    }
 	    else {
 		$this->_end("invalid status line: $line");
@@ -173,10 +190,13 @@ sub _main {
 	    if (length $line == 0) {
 		# ヘッダ終わり
 		$this->{header_fetched} = 1;
+		$this->{reply}{StreamState} = 'fetch_body';
 	    }
 	    else {
-		if ($line =~ m|(.+?): (.+)$|) {
-		    $this->{reply}{Header}{$1} = $2;
+		if ($line =~ m|(.+?): ?(.+)$|) {
+		    my ($key, $val) = ($1, $2);
+		    $key =~ /^Content-Type\z/i and $key = 'Content-Type';
+		    $this->{reply}{Header}{$key} = $val;
 		}
 		else {
 		    $this->_end("invalid header line: $line");
@@ -190,16 +210,31 @@ sub _main {
 	}
     }
 
+    if( $this->{header_fetched} && $this->{socket}->recvbuf )
+    {
+      #$DEBUG and print "<< (merge body)\n" . $this->{socket}->recvbuf."<< (end)\n";
+      $progress = 1;
+      $this->{reply}{Content} .= $this->{socket}->recvbuf;
+      $this->{socket}->recvbuf = '';
+    }
+
     # 切断されていたら、ここで終わり。
     if (!$this->{socket}->connected) {
+        $DEBUG and print "<< (disconnected)\n";
 	if (!$this->{status_fetched} or
 	      !$this->{header_fetched}) {
 	    $this->_end("unexpected disconnect by server");
 	}
 	else {
-	    $this->{reply}{Content} .= $this->{socket}->recvbuf;
+	    $this->{reply}{StreamState} = 'finished';
 	    $this->_end;
 	}
+    }else
+    {
+      if( $progress && $this->{progress_callback} )
+      {
+        $this->{progress_callback}->($this->{reply});
+      }
     }
 }
 
@@ -224,7 +259,7 @@ sub alive_p {
 sub stop {
     my $this = shift;
 
-    $this->{socket}->disconnect if $this->{socket};
+    $this->{socket}->disconnect if $this->{socket} && $this->{socket}->connected;
     $this->{hook}->uninstall if $this->{hook};
     $this->{timeout_timer}->uninstall if $this->{timeout_timer};
 
@@ -235,3 +270,101 @@ sub stop {
 }
 
 1;
+
+=encoding euc-jp
+
+=head1 NAME
+
+Tools::HTTPClient - HTTP Client
+
+=head1 SYNOPSIS
+
+ use Tools::HTTPClient;
+
+ my $http = Tools::HTTPClient->new(
+   Url    => 'http://www.example.com',
+   Method => 'GET',
+ );
+ $http->start(\&callback);
+
+=head1 DESCRIPTION
+
+HTTP Client for tiarra.
+
+ブロックしないように処理は非推奨なので,
+ブロックしないように調整されている HTTP 
+クライアントモジュール.
+
+=head1 METHODS
+
+=head2 new
+
+ my $http = Tools::HTTPClient->new(
+   Url    => 'http://www.example.com',
+   Method => 'GET',
+ );
+
+インスタンスの生成.
+C<Url> 及び C<Method> 引数は必須.
+
+その他の省略可能な引数として,
+C<Content> (POST内容),
+C<Header>  (HASH-ref),
+C<Timeout> (秒単位)
+が利用可能.
+
+=head2 start
+
+ $http->start(\&callback);
+ $http->start(%opts);
+
+ $opts{Callback}         = \&callback;
+ $opts{ProgressCallback} = \&progress_callback;
+
+C<\&callback> は処理完了時に呼ばれる関数.
+C<\&progress_callback> は処理の進捗があったときに呼ばれる関数.
+
+C<\&callback> は, HTTPが正常に完了すればHASH-refを,
+タイムアウトやエラー時にはエラー内容を含んだ文字列を
+引数として呼び出される.
+
+  sub my_callback {
+    my $response = shift;
+    if( !ref($response) )
+    {
+      # error.
+      return;
+    }
+    # success.
+    my $protocol    = $response->{Protocol};
+    my $status_code = $response->{Code};
+    my $status_msg  = $response->{Message};
+    my $headers     = $response->{Header}; # hash-ref.
+    my $content     = $response->{Content};
+  }
+
+C<\&progress_callback> も同様, 
+ただしこちらはエラーの報告には呼ばれない.
+
+=head2 stop
+
+ $http->stop();
+
+リクエストの終了.
+このときは L</start> で指定した C<\&callback> がよばれないので注意.
+
+=head1 AUTHOR
+
+phonohawk <phonohawk@ps.sakura.ne.jp>
+
+=head1 COPYRIGHT
+
+Copyright 2004 phonohawk  <phonohawk@ps.sakura.ne.jp>
+
+=head1 SEE ALSO
+
+tiarra
+http://coderepos.org/share/wiki/Tiarra
+(Japanese)
+
+=cut
