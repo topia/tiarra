57f9e071e0f35f4e31b7846455480af188028ced
ha1t <devnull@localhost>
2008-05-10 05:52:44 +0000

    チャンネル名が出るようにした

--- module/Auto/Im.pm
+++ module/Auto/Im.pm
@@ -42,15 +42,17 @@ sub message_arrived {
   if ($sender->isa('IrcIO::Server')) {
       # PRIVMSGか？
       if ($msg->command eq 'PRIVMSG') {
-          my ($get_ch_name,undef,undef,$reply_anywhere)
+          my ($get_ch_name,$reply_in_ch,$reply_as_priv,$reply_anywhere, $get_full_ch_name)
           = Auto::Utils::generate_reply_closures($msg,$sender,\@result);
+          
+          my $full_ch_name = $get_full_ch_name->();
 
           my ($str, $who, $text) = split(/\s+/, $msg->param(1), 3);
 
           if (Mask::match_deep([$this->config->keyword('all')], $str)) {
               # 一致していた。
               LWP::UserAgent->new->request( POST "http://im.kayac.com/api/post/$this->{user}",
-                  [ message => "[tiarra] $str $who $text" ]
+                  [ message => "[tiarra][$full_ch_name] $str $who $text" ]
               );
               #$this->_send($msg, $sender, $who, $text, $get_ch_name, $reply_anywhere);
           }
