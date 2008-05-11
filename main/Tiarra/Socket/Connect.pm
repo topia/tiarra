7b937879344f277f770d2808ea9dbaf9e0ce0c56
Topia <topia@clovery.jp>
2008-05-11 13:59:27 +0000

    * 動作フローの見直し。主に error から warn and try next への変更。 * hooks->{before_connect} を追加(したけど未テスト)。   * 特定のaddr/port/type をみて skip したり、書き換えたりできるはず。 * bind_addr をコネクション毎にオーバーライドできるように(同じく未テスト)。 * callback の optional genre として skip を追加した。 * ドキュメントをちょっと書き直した。   * 重要なのは callback の 'error' genre は接続の試行が終わるというところ。

--- main/Tiarra/Socket/Connect.pm
+++ main/Tiarra/Socket/Connect.pm
@@ -15,7 +15,8 @@ use Tiarra::OptionalModules;
 use Tiarra::Utils;
 utils->define_attr_accessor(0, qw(domain host addr port callback),
 			    qw(bind_addr prefer timeout),
-			    qw(retry_int retry_count try_count));
+			    qw(retry_int retry_count try_count),
+			    qw(hooks));
 utils->define_attr_enum_accessor('domain', 'eq',
 				 qw(tcp unix));
 
@@ -28,26 +29,31 @@ utils->define_attr_enum_accessor('domain', 'eq',
 #       callback => sub {
 #           my ($genre, $connector, $msg_or_sock, $errno) = @_;
 #           if ($genre eq 'warn') {
-#               # $msg_or_sock: msg
-#               # maybe don't have $errno
+#               # warnings
+#               #   $msg_or_sock: msg
 #               warn $msg_or_sock;
 #           } elsif ($genre eq 'error') {
-#               # $msg_or_sock: msg
-#               # maybe has $errno
+#               # error: error detected and connect attempt aborted.
+#               #        if you need, re-initialize connect.
+#               #   $msg_or_sock: msg
 #               die $msg_or_sock;
 #           } elsif ($genre eq 'sock') {
-#               # $msg_or_sock: sock
-#               # maybe don't have $errno
+#               # connection successful. return socket.
+#               #   $msg_or_sock: sock
 #               attach($connector->current_addr, $connector->current_port,
 #                      $msg_or_sock);
 #           # optional genre
+#           } elsif ($genre eq 'skip') {
+#               # skipped by hook closure
+#               #   $msg_or_sock: msg
+#               warn $msg_or_sock;
 #           } elsif ($genre eq 'interrupt') {
-#               # $msg_or_sock: undef
-#               # maybe don't have $errno
+#               # connection interrupted by user
+#               #   $msg_or_sock: undef
 #               die 'interrupted';
 #           } elsif ($genre eq 'timeout') {
-#               # $msg_or_sock: undef
-#               # maybe don't have $errno
+#               # connection interrupted by timer
+#               #   $msg_or_sock: undef
 #               die 'timeout';
 #           }
 #       },
@@ -60,6 +66,21 @@ utils->define_attr_enum_accessor('domain', 'eq',
 #       prefer => [prefer socket type(and order) (ipv4, ipv6) as string's
 #                  array ref, default ipv6, ipv4],
 #       domain => 'tcp', # default
+#       hooks => {
+#           before_connect => {
+#               my ($stage, $connecting) = @_;
+#               # hook before connection attempt
+#               #   $stage: 'before_connect'
+#               #   $connecting: { # be able to modify this hash.
+#               #       addr => $addr, port => $port,
+#               #       type => 'ipv4' or 'ipv6',
+#               #       bind_addr => undef or set connection-local bind_addr,
+#               #   }
+#               if ( /* not_want_to_connect */ ) {
+#                   die 'skip this connection';
+#               }
+#           }
+#       }
 #       );
 #   $connector->interrupt;
 # unix:
@@ -79,7 +100,7 @@ sub new {
     my $this = $class->SUPER::new(%opts);
     map {
 	$this->$_($opts{$_});
-    } qw(host addr port callback bind_addr timeout retry_int retry_count);
+    } qw(host addr port callback bind_addr timeout retry_int retry_count hooks);
 
     if (!defined $this->callback) {
 	croak 'callback closure required';
@@ -199,10 +220,12 @@ sub _try_connect_ipv6 {
     my $this = shift;
 
     if (!Tiarra::OptionalModules->ipv6) {
-	$this->_error(
+	$this->_warn(
 	    qq{Host $this->{host} seems to be an IPv6 address, }.
 		qq{but IPv6 support is not enabled. }.
 		    qq{Use IPv4 or install Socket6 or IO::Socket::INET6 if possible.\n});
+	$this->_connect_try_next;
+	return;
     }
 
     $this->_try_connect_tcp('IO::Socket::INET6');
@@ -211,14 +234,26 @@ sub _try_connect_ipv6 {
 sub _try_connect_tcp {
     my ($this, $package, $addr, %additional) = @_;
 
-    if (!eval("require $package")) {
-	$this->_connect_error("Couldn\'t require socket package: $package");
+    eval {
+	$this->_call_hooks('before_connect', $this->{connecting});
+    }; if ($@) {
+	$this->_call_skip($@);
+	$this->_connect_try_next;
 	return;
     }
+    if (defined $this->{hooks}->{before_connect}) {
+	#eval { $this->{hooks}->{before_connect}->('before_connect', ) }
+    }
+    if (!eval("require $package")) {
+	$this->_connect_warn("Couldn\'t require socket package: $package");
+	$this->_connect_try_next;
+	return;
+    }
+    my $bind_addr = $this->current_bind_addr;
     my $sock = $package->new(
 	%additional,
-	(defined $this->{bind_addr} ?
-	     (LocalAddr => $this->{bind_addr}) : ()),
+	(defined $bind_addr ?
+	     (LocalAddr => $bind_addr) : ()),
 	Timeout => undef,
 	Proto => 'tcp');
     if (!defined $sock) {
@@ -272,6 +307,7 @@ sub _try_connect_unix {
 	    qq{Address $this->{addr} seems to be an Unix Domain Socket address, }.
 		qq{but Unix Domain Socket support is not enabled. }.
 		    qq{Use other protocol if possible.\n});
+	return;
     }
 
     require IO::Socket::UNIX;
@@ -334,6 +370,14 @@ sub current_port {
 	$this->port);
 }
 
+sub current_bind_addr {
+    my $this = shift;
+
+    utils->get_first_defined(
+	$this->{connecting}->{bind_addr},
+	$this->bind_addr);
+}
+
 sub current_type {
     my $this = shift;
 
@@ -359,6 +403,26 @@ sub _call {
     my $this = shift;
 
     $this->callback->('sock', $this, $this->sock);
+}
+
+sub _call_skip {
+    # this address/port skipped; but continue trying
+    my ($this, $str, $errno) = @_;
+
+    $this->callback->('skip', $this,
+		      "skip connection attempt to ".$this->destination.$str,
+		      $errno);
+}
+
+sub _call_hooks {
+    # method may died by callback.
+    # please cover with eval, if you need.
+    my $this = shift;
+    my $genre = shift;
+
+    if (defined $this->{hooks}->{$genre}) {
+	$this->{hooks}->{$genre}->($genre, @_);
+    }
 }
 
 sub cleanup {
